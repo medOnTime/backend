e0e7fa912380bd13fb7a5d60459c0b2badc3216d
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2024-11-23 20:56:52 +0530

    change project folder structure

---
+++ medicineService/src/test/java/com/medOnTime/medicineService/MedicineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medOnTime.medicineService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
