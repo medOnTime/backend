892a708764e5b4ee052757a90000826b1625bc05
lathikaarosha <lathikaarosha123@gmail.com>
2025-06-03 15:12:09 +0530

    Api gateway for communication with the services

--- medicineService/src/main/java/com/medOnTime/medicineService/controller/medicineController.java
+++ medicineService/src/main/java/com/medOnTime/medicineService/controller/medicineController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("medicine_service")
+@RequestMapping("medicine")
 public class medicineController {
 
     @Autowired
--- userManagementService/src/main/java/com/MedOnTime/userManagementService/controller/UserManagementController.java
+++ userManagementService/src/main/java/com/MedOnTime/userManagementService/controller/UserManagementController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("userManagementService")
+@RequestMapping("user")
 public class UserManagementController {
 
     @Autowired
